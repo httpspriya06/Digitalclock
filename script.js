0717358098ba16b3d7109888e526b94f46b21b90
priyasingh <141578358+httpspriya06@users.noreply.github.com>
2023-09-24 13:09:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+setInterval(() => {
+  d = new Date();
+  htime = d.getHours();
+  mtime = d.getMinutes();
+  stime = d.getSeconds();
+  hrotation = 30 * htime + mtime / 2;
+  mrotation = 6 * mtime;
+  srotation = 6 * stime;
+
+  hour.style.transform = `rotate(${hrotation}deg)`;
+  minute.style.transform = `rotate(${mrotation}deg)`;
+  second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
+
+// ............................
+function updateClock() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const dayOfWeek = daysOfWeek[now.getDay()];
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const monthName = monthNames[now.getMonth()];
+  const dayOfMonth = now.getDate().toString().padStart(2, "0");
+
+  const clock2 = document.getElementById("date");
+  const clock3 = document.getElementById("time");
+  clock3.textContent = `${hours}:${minutes}`;
+  clock2.textContent = `${dayOfWeek}, ${monthName} ${dayOfMonth}`;
+}
+setInterval(updateClock, 60000);
+updateClock();
+// .........................................................
+const toggle = document.querySelector(".toggle");
+toggle.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Dark mode";
+  } else {
+    html.classList.add("dark");
+    e.target.innerHTML = "Light mode";
+  }
+});
